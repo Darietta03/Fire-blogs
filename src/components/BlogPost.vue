a7a24df1e1c974cc1f7494ae2623be5b05d687b8
Darietta03 <dsnegireva2017@gmail.com>
2023-01-02 22:35:52 +0200

    changes02/01/23

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="blog-wrapper">
+    <div class="blog-wrapper no-user">
         <div class="blog-content">
             <div>
                 <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
@@ -9,7 +9,7 @@
                 <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
                     Login/Register<Arrow class="arrow arrow-light"/>
                 </router-link>
-                <router-link class="link link-light" v-else to="#">
+                <router-link class="link" v-else to="#">
                     View The Post<Arrow class="arrow"/>
                 </router-link>
             </div>
@@ -61,7 +61,7 @@ export default {
         }
 
         div {
-            min-width: 375px;
+            width: 375px;
             padding: 72px 24px;
             @media (min-width: 700px) {
             padding: 0 24px;
@@ -91,9 +91,63 @@ export default {
                 overflow: hidden;
                 text-overflow: ellipsis;
             }
+
+            .link {
+                display: inline-flex;
+                align-items: center;
+                margin-top: 32px;
+                padding-bottom: 4px;
+                border-bottom: 1px solid transparent;
+                transition: .5s ease-in all;
+
+                &:hover {
+                    border-bottom-color: #303030;
+                }
+            }
+
+            .link-light {
+                &:hover {
+                    border-bottom-color: #fff;
+                }
+            }
         }
 
 
+        }
+
+        .blog-photo {
+            order: 1;
+            flex: 3;
+            box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+            @media (min-width: 700px) {
+            order: 2;
+            }
+            @media (min-width: 800px) {
+            flex: 4;
+            }
+
+            img {
+                display: block;
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+
+        &:nth-child(even) {
+            .blog-content {
+                order: 2;
+            }
+            .blog-photo {
+                order: 1;
+            }
+        }
+    }
+
+    .no-user:first-child {
+        .blog-content {
+            background-color: #303030;
+            color: #fff;
         }
     }
 </style>
