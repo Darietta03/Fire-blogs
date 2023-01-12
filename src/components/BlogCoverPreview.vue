7320e2c13e210462d64a1ba8ac38d61c9d27b70c
Darietta03 <dsnegireva2017@gmail.com>
2023-01-12 22:33:44 +0200

    12/01/23

---
+++ src/components/BlogCoverPreview.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="modal" ref="modal">
+        <div class="modal-content">
+            <close @click="closePreview" class="icon" />
+            <img :src="this.blogCoverPhoto" alt="" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { throws } from "assert";
+import close from "../assets/Icons/times-circle-light.svg"
+
+export default {
+    components: {
+        close,
+    },
+
+    methods: {
+        closePreview() {
+            this.$store.commit("openPhotoPreview");
+        },
+    },
+
+    computed: {
+        blogCoverPhoto() {
+            return this.$store.state.blogPhotoFileURL
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -2,19 +2,19 @@
     <div class="create-post">
         <div class="container">
             <div :class="{invisible: !error}" class="err-message">
-                <p><span>Erro:</span>{{ this.errorMsg }}</p>
+                <p><span>Error:</span>{{ this.errorMsg }}</p>
             </div>
             <div class="blog-info">
                 <input type="text" placeholder="Enter Blog Title" v-model="blogTitle"/>
                 <div class="upload-file">
                     <label for="blog-photo">Upload Cover Photo</label>
-                    <input type="file" ref="blogPhoto" id="blog-photo" accept=".png, .jpg, .jpeg"/>
-                    <button class="preview" :class="{'button-inactive': this.$store.state.blogPhotoFileURL}">Preview Photo</button>
+                    <input type="file" ref="blogPhoto" id="blog-photo" @change="fileChange" accept=".png, .jpg, .jpeg"/>
+                    <button class="preview" :class="{'button-inactive': this.$store.state.blogPhotoFileURL }">Preview Photo</button>
                     <span>File Chosen: {{ this.$store.state.blogPhotoName }}</span>
                 </div>
             </div>
             <div class="editor">
-                <vue-editor :editorOptions="editorSettiings" v-model="blogHTML" useCustomImageHandler />
+                <vue-editor :editorOptions="editorSettings" v-model="blogHTML" useCustomImageHandler />
             </div>
             <div class="blog-actions">
                 <button class="button">Publish Blog</button>
@@ -25,6 +25,7 @@
 </template>
 
 <script>
+
 import Quill from "quill";
 window.Quill = Quill;
 const ImageResize = require("quill-image-resize-module").default;
@@ -35,16 +36,49 @@ Quill.register("modules/imageResize", ImageResize);
 
         data() {
             return {
+                file: null,
                 error: null,
                 errorMsg: null,
                 editorSettiings: {
                     modules: {
                         imageResize: {},
-                    }
-                }
+                    },
+                },
+            };
+        },
+        methods: {
+            fileChange() {
+                this.file = this.$refs.blogPhoto.files[0];
+                const fileName = this.file.name;
+                this.$store.commit("fileNameChange", fileName);
+                this.$store.commit("createFileURL", URL.createObjectURL(this.file));
             }
-        }
-    };
+        },
+        computed: {
+            profileId() {
+                return this.$store.state.profileId;
+            },
+            blogCoverPhotoName() {
+                return this.$store.state.blogPhotoName;
+            },
+            blogTitle: {
+                get() {
+                    return this.$store.state.blogTitle;
+                },
+                set(payload) {
+                    this.$store.commit("updateBlogTitle", payload);
+                },
+            },
+            blogHTML: {
+                get() {
+                    return this.$store.state.blogHTML;
+                },
+                set(payload) {
+                    this.$store.commit("newBlogPost", payload);
+                },
+            },
+        },
+};
 </script>
 
 <style lang="scss">
@@ -108,6 +142,76 @@ Quill.register("modules/imageResize", ImageResize);
             font-weight: 600;
         }
     }
+    .blog-info {
+        display: flex;
+        margin-bottom: 32px;
 
+        input:nth-child(1){
+            min-width: 300px;
+        }
+
+        input {
+            transition: 0.5s ease-in-out all;
+            padding: 10px 4px;
+            border: none;
+            border-bottom: 1px solid #303030;
+
+            &:focus {
+                outline: none;
+                box-shadow: 0 1px 0 0 #303030;
+            }
+        }
+
+        .upload-file {
+            flex: 1;
+            margin-left: 16px;
+            position: relative;
+            display: flex; 
+
+            input {
+                display: none;
+            }
+
+            .preview {
+                margin-left: 16px;
+                text-transform: initial;
+            }
+            span {
+                font-size: 12px;
+                margin-left: 16px;
+                align-self: center;
+            }
+
+        }
+    }
+
+    .editor {
+        height: 60vh;
+        display: flex;
+        flex-direction: column;
+
+        .quillWrapper {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+        .ql-container {
+                display: flex;
+                flex-direction: column;
+                height: 100%;
+                overflow: scroll;
+        }
+        .ql-editor {
+            padding: 20px 16px 30px;
+        }
+    }
+    .blog-actions {
+        margin-top: 32px;
+
+        button {
+            margin-right: 16px;
+        }
+    }
 }
 </style>
